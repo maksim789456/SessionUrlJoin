09a58785cf2345855419398c210d67de8a55b74c
maksim789456 <maksim_789456@mail.ru>
2023-04-06 01:46:13 +0400

    First version

---
+++ README.md
@@ -0,0 +1,10 @@
+# SessionUrlJoin
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allow join in session by paste session url from clipboard.
+
+Support **lnl://**, **lnl-nat://** and **neos-steam://** session url's.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [SessionUrlJoin.dll](https://github.com/maksim789456/SessionUrlJoin/releases/latest/download/SessionUrlJoin.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ SessionUrlJoin/SessionUrlJoinMod.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace SessionUrlJoin
+{
+    public class SessionUrlJoinMod : NeosMod
+    {
+        public override string Name => "SessionUrlJoin";
+        public override string Author => "maksim789456";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            var harmony = new Harmony("me.maksim789456.SessionUrlJoin");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(BatchFolderImporter), "BatchImport", typeof(Slot), typeof(IEnumerable<string>), typeof(bool))]
+        class BatchFolderImporter_BatchImport_Patch
+        {
+            static bool Prefix(Slot root, IEnumerable<string> files)
+            {
+                IEnumerable<string> rawUrls = files.Where(x =>
+                    x.StartsWith("lnl-nat://") || x.StartsWith("neos-steam://") || x.StartsWith("lnl://"));
+
+                List<Uri> urls = new List<Uri>();
+                foreach (var rawUrl in rawUrls)
+                {
+                    try
+                    {
+                        var url = new Uri(rawUrl);
+                        urls.Add(url);
+                    }
+                    catch (Exception e)
+                    {
+                        Warn($"Url {rawUrl} cannot parse. Error message: {e.Message}");
+                    }
+                }
+
+                if (urls.Count == 0)
+                {
+                    Warn("Don't valid urls");
+                    return true;
+                }
+
+                root.World.Coroutines.StartTask(async () =>
+                {
+                    LoadingIndicator loadingIndicator = await LoadingIndicator.CreateIndicator();
+
+                    await Userspace.OpenWorld(new WorldStartSettings()
+                    {
+                        URIs = urls,
+                        GetExisting = true,
+                        Relation = Userspace.WorldRelation.Nest,
+                        LoadingIndicator = loadingIndicator
+                    });
+                    await Task.Delay(1000);
+                });
+                return false;
+            }
+        }
+    }
+}
