1254adb56165c03debddb1fc2150d7993bfc7e0f
maksim789456 <maksim_789456@mail.ru>
2023-10-18 22:43:23 +0400

    Add Assembly info

---
+++ SessionUrlJoin/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SessionUrlJoin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("maksim789456")]
+[assembly: AssemblyProduct("SessionUrlJoin")]
+[assembly: AssemblyCopyright("Copyright © maksim789456 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
